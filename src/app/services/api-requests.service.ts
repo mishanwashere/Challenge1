9b7f70db89c08fba1defaa1cd8d7b09d0571bc2f
mishanwashere <mishangroenewald@gmail.com>
2022-07-20 15:19:02 +0200

    feat(api-requests.service): update API request function name, and set response data to class variable('currentSearchedArtists').

--- src/app/services/api-requests.service.ts
+++ src/app/services/api-requests.service.ts
@@ -6,14 +6,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiRequestsService {
 
+  private currentSearchedArtists: any = null;
+
   constructor(
     private http: HttpClient,
   ) { }
 
-  getArtist() {
+  getArtistRequest(searchValue: string) {
     const proxyUrl = "http://cors-anywhere.herokuapp.com/";
     const apiUrl = "https://api.deezer.com/";
 
-    return this.http.get(proxyUrl + apiUrl + "artist/eminem");
+    this.http.get(proxyUrl + apiUrl + "artist/" + searchValue).subscribe(responseData => {
+      this.currentSearchedArtists = responseData;
+    });
   }
 }
