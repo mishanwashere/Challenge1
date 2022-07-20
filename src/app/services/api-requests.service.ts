45a345c645c15c19a9ca6f2ebf1337e9c1b332cf
mishanwashere <mishangroenewald@gmail.com>
2022-07-20 23:10:09 +0200

    feat(api-requests.service): added rudimentary circuit breaker pattern, also correctly type functions.

--- src/app/services/api-requests.service.ts
+++ src/app/services/api-requests.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Subject, Observable } from 'rxjs';
 
 import { ApiRequestEnum } from './model/api-request.enum';
@@ -11,25 +11,48 @@ import { ApiResponseModel } from './model/api-response.model';
 export class ApiRequestsService {
 
   private currentSearchedArtists: Subject<ApiResponseModel> = new Subject<ApiResponseModel>();
+  private errorCounter: number = 0;
+  private circuitStatus: string = "Closed";
 
   constructor(
     private http: HttpClient,
   ) { }
 
-  getArtistsRequest(searchValue: string) {
-    const proxyUrl = ApiRequestEnum.ProxyUrl;
-    const apiUrl = ApiRequestEnum.ApiUrl;
+  public getArtistsRequest(searchValue: string): void {
+    // Rudimentary Circuit Breaker Pattern.
+    if (this.circuitStatus === "Open") {
+      return;
+    }
 
-    this.http.get<ApiResponseModel>(proxyUrl + apiUrl + "artist/" + searchValue).subscribe((responseData: ApiResponseModel) => { // Could probably clean up this built up URL.
+    if (this.errorCounter >= 2) {
+      this.circuitStatus = "Open";
+
+      console.error("Open circuit! API failed 3 times. Timeout for 15 seconds.");
+      setTimeout(() => {
+        this.errorCounter = 0;
+        this.circuitStatus = "Closed";
+        console.info("Closed Circuit! Retrying.");
+      }, 15000);
+    }
+    // END
+
+    this.http.get<ApiResponseModel>(ApiRequestEnum.ProxyUrl + ApiRequestEnum.ApiUrl + "artist/" + searchValue).subscribe((responseData: ApiResponseModel) => { // Could probably clean up this built up URL.
+      if (responseData?.error?.code == 800) { // API doesn't return a 404 not found. Rudimentary error handling for no API response data.
+        this.errorCounter++;
+        return;
+      }
+
+      this.errorCounter = 0;
+      this.circuitStatus = "Closed";
       this.setArists(responseData); // call setter.
     });
   }
 
-  getArtists(): Observable<ApiResponseModel> { // getter.
+  public getArtists(): Observable<ApiResponseModel> { // getter.
     return this.currentSearchedArtists.asObservable(); // returns observable.
   }
 
-  setArists(responseData: ApiResponseModel): void { // setter.
+  private setArists(responseData: ApiResponseModel): void { // setter.
     this.currentSearchedArtists.next(responseData); // update subject
   }
 }
